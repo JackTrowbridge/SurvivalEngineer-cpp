b516106f7ff0baeb48003f3b727a35ca61f58d8b
Jack Trowbridge <jacktrowbridge2004@gmail.com>
2024-02-27 08:07:24 +0000

    Prep for actual dependency management

--- src/window/WindowManager.cpp
+++ src/window/WindowManager.cpp
@@ -1,12 +1,10 @@
-#include "raylib.h"
-#include "rlImGui.h"
-#include "imgui.h"
-#include "WindowManager.h"
+#include "WindowManager.hpp"
 
 WindowManager::WindowManager(int width, int height, const char* title)
 {
 	InitWindow(width, height, title);
 	rlImGuiSetup(true);
+
 	SetTargetFPS(60);
 
 	while (!WindowShouldClose())
@@ -26,6 +24,17 @@ void WindowManager::Draw()
 {
 	BeginDrawing();
 	ClearBackground(BLACK);
+
+	BeginMode2D(WindowManager::camera);
+	
+	rlPushMatrix();
+		rlTranslatef(0, 25*50, 0);
+		rlRotatef(90, 1, 0, 0);
+		DrawGrid(100, 50);
+	rlPopMatrix();
+
+	EndMode2D();
+
 	drawImGui();
 }
 
---
+++ src/window/WindowManager.hpp
@@ -0,0 +1,25 @@
+#ifndef WINDOWMANAGER_H
+#define WINDOWMANAGER_H
+
+#include "raylib.h"    // Base raylib functionality
+#include "raymath.h"   // Vector and matrix operations, dependent on raylib
+#include "rlgl.h"      // Low-level graphics layer, dependent on raylib
+#include "imgui.h"     // Dear ImGui library
+#include "rlImGui.h"   // Integration of raylib with ImGui, dependent on both
+
+class WindowManager {
+public:
+	WindowManager(int width, int height, const char* title);
+	~WindowManager();
+
+	Camera2D camera = { 0 };
+
+	void Draw();
+	void EndDraw();
+
+	void drawImGui();
+	void endImGui();
+private:
+};
+
+#endif
